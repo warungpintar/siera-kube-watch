48dfd4c6aa7898665ad2e5e2e660ed702a212292
Rika Dewi <37909642+Rikadewi@users.noreply.github.com>
2020-06-02 20:24:12 +0700

    Post event to slack (#3)
    Co-authored-by: Agung Julisman <agungjulisman19@gmail.com>

--- util/helper.go
+++ util/helper.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"github.com/warungpintar/siera-kube-watch/config"
 	corev1 "k8s.io/api/core/v1"
-	"log"
 )
 
 func NotifyEvent(event *corev1.Event) {
 	if !isExist(config.GlobalConfig.ExcludedReasons, event.Reason) {
 		if event.Type != NORMAL {
 			message := parseEventToMessage(event)
-			log.Println(message)
 			postEvent(message)
 		} else {
 			if event.Reason == SCALING_REPLICAT_SET || event.Reason == STARTED || event.Reason == KILLING {
@@ -24,12 +22,7 @@ func NotifyEvent(event *corev1.Event) {
 }
 
 func parseEventToMessage(event *corev1.Event) string {
-	return fmt.Sprintf("[%s] %s/%s %s", event.Reason, event.Namespace, event.Name, event.Message)
-}
-
-func NotifyPod(pod *corev1.Pod) {
-	message := fmt.Sprintf("%s/%s Phase: %s Reason: %s Message: %s", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, pod.Status.Phase, pod.Status.Reason, pod.Status.Message)
-	log.Println(message)
+	return fmt.Sprintf("[%s: %s] %s/%s %s", event.Type, event.Reason, event.Namespace, event.Name, event.Message)
 }
 
 func isExist(arr []string, element string) bool {
